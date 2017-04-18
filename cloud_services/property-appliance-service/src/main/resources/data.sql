76f5150026abb5eb4e6153fa4c8daeca90838c4f
Cit-WKyle <kyle.williamson@mycit.ie>
2017-04-18 16:13:55 +0100

    Service Commit - Fix

--- cloud_services/property-appliance-service/src/main/resources/data.sql
+++ cloud_services/property-appliance-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO status_history(id, status_id, logged_time) SELECT 1, 1, '2017-01-01 01:01:01' WHERE NOT EXISTS(SELECT 1 FROM status_history WHERE id=1)
 
-INSERT INTO property_appliances(id, appliance_id, property_id, status_id, name, age) SELECT 1, '58f539c6faccfd35d35166bb', 1, 6, 'Kitchen Dishwasher', 5 WHERE NOT EXISTS(SELECT 1 FROM property_appliances WHERE id=1)
+INSERT INTO property_appliances(id, appliance_id, property_id, status_id, name, age) SELECT 1, '58f539c6faccfd35d35166bb', 1, 2, 'Kitchen Dishwasher', 5 WHERE NOT EXISTS(SELECT 1 FROM property_appliances WHERE id=1)
 INSERT INTO property_appliances(id, appliance_id, property_id, status_id, name, age) SELECT 2, '58f539c6faccfd35d35166bc', 1, 1, 'PropertyAppliance2', 5 WHERE NOT EXISTS(SELECT 1 FROM property_appliances WHERE id=2)
 INSERT INTO property_appliances(id, appliance_id, property_id, status_id, name, age) SELECT 3, '58f539c6faccfd35d35166bd', 1, 2, 'PropertyAppliance3', 5 WHERE NOT EXISTS(SELECT 1 FROM property_appliances WHERE id=3)
 
